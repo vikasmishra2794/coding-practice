3f3b1b1d1e1184f849e5e3d5873b8c879b033bed
Vikas Mishra <vikash.msihra@pens.com>
2023-01-11 00:33:17 +0530

    - Added the react code.

--- projects/js/test.js
+++ projects/js/test.js
@@ -1,5 +1,24 @@
-function foo(a,b) {
-    return a + b;
-}
-
-console.log(foo(2, 3));
+const testObject = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: {
+          f: []
+        }
+      }
+    }
+  }
+  function flattenedObj(obj) {
+    const flat = {};
+    Object.keys(obj).forEach(key => {
+        const val = obj[key];
+        if (val == 'object' && val != null && !Array.isArray(val)) {
+            flat = {...flat, ...flattenedObj(val)};
+        } else {
+            flat[key] = obj[key];
+        }
+    });
+    return flat;
+  }
+  console.log(flattenedObj(testObject));
--- projects/react-code/my-app/src/App.js
+++ projects/react-code/my-app/src/App.js
@@ -1,22 +1,62 @@
-import React, { useCallback, useEffect, useReducer, useRef, useState } from "react"
-import axios from 'axios';
-import A from './A';
-import B from './B';
-import { act } from "react-dom/test-utils";
+import React from "react"
 
-export const MyContext = React.createContext({});
-  const App = (props) => {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const myRef = useRef();
-  const buttonClick = () => {
-    console.log("myRef.current:===", myRef.current);
-    clearInterval(myRef.current);
+  const App = () => {
+    // Sample input
+    const payload = {
+    "columns":["col1","col2","col3","col4","col5","col6"],
+    "rows":[
+      [{
+        "cell_id": 1,
+        "value": "abc"
+      }, {
+        "cell_id": 2,
+        "value": "abc"
+      }, {
+        "cell_id": 3,
+        "value": "abc"
+      }, {
+        "cell_id": 4,
+        "value": "xyz"
+      }, {
+        "cell_id": 5,
+        "value": "xyz"
+      }, {
+        "cell_id": 6,
+        "value": "ghf"
+      }
+  ]
+]}
+
+  const getColspan = (arr) => {
+    const obj = {};
+    for(let data of arr) {
+      if (obj[data.value]) {
+        obj[data.value]++;
+      } else {
+        obj[data.value] = 1;
+      }
+    }
+    return obj;
   }
+  
   return (
     <div>
-      <div>sadjfsdgj</div>
-      <div>viksdkskdksdks</div>
+      <table border={2}>
+        {
+          //map to traverse each row.
+          payload.rows.map(data => {  
+            const obj = getColspan(data);
+            //map to traverse data in each row.
+            return <tr>{Object.keys(obj).map(key => {
+              return <td colSpan={obj[key]}>{key}</td>
+            })}
+          </tr>
+        })
+        }
+        <tr>
+          {payload.columns.map(data => <td>{data}</td>)}
+        </tr>
+      </table>
     </div>
   )
 };
